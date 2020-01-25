6758e53576232ffde2fa260393e01921a8f8e9c0
Iwo Polański <outlying@gmail.com>
2020-01-25 19:55:27 +0100

    Run provided commands

--- sensor.py
+++ sensor.py
@@ -11,10 +11,14 @@ import os
 parser = argparse.ArgumentParser(description='PIR motion sensor script for keeping screen on and off')
 parser.add_argument("-t", default=300, metavar='seconds', type=int, help="Time before screen goes off, default: 300 seconds")
 parser.add_argument("-p", default=16, metavar='pin_number', type=int, help="Pin used to collect motion sensor data, default: 16")
+parser.add_argument("-o", required=True, metavar='on_command', help="Execute for when moved detected")
+parser.add_argument("-f", required=True, metavar='off_command', help="Execute after timeout")
 
 args = parser.parse_args()
 timeUntilScreenOff = args.t
 pirPin = args.p
+cmdOn = args.o
+cmdOff = args.f
 
 # Prepare logger
 logger = logging.getLogger('Sensor')
@@ -49,9 +53,9 @@ while True:
                 logger.info('Motion detected, timer reset')
                 timer = 0
             if not screenOn:
-                logger.info('Turn screen on')
+                logger.info('Execute ON')
                 screenOn = True
-                os.system("tvservice -p && sudo chvt 1 && sudo chvt 7")
+                os.system(cmdOn)
                 # execute cli screen on
     elif state == 0 and timer < timeUntilScreenOff:
         timer = timer + 1
@@ -60,9 +64,9 @@ while True:
         if timer == timeUntilScreenOff:
             logger.info('Timer reached ' + str(timer) + ' seconds')
             if screenOn:
-                logger.info('Turn screen off')
+                logger.info('Execute OFF')
                 screenOn = False
-                os.system("tvservice -o")
+                os.system(cmdOff)
 
     previousState = state
 
